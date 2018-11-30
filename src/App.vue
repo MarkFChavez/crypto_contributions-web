64f9c9729941ff8a2e98511cc8622af3ab60aaaa
Mark Chavez <markchav3z@gmail.com>
2018-12-01 01:12:40 +0800

    Add and design the application header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="app">
-    Hello world
+  <div class='container px-2 py-8'>
+    <Header />
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  components: { Header }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='border-b-2 border-black py-4'>
+    <p class='bungee text-4xl'> Crypto contributions </p>
+    <span class='leading-loose'>
+      Made and developed by 
+      <a 
+        href='https://twitter.com/MarkFChavez' 
+        target='_blank'
+        class='font-bold text-black no-underline'> 
+        @MarkFChavez 
+      </a>
+    </span>
+  </div>
+</template>
