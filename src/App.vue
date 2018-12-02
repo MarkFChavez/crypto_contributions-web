0fa9ff741ed078a9e6292387c8c7ab7044506133
Mark Chavez <markchav3z@gmail.com>
2018-12-03 00:13:16 +0800

    Redesign content

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <div class='container px-2 py-2'>
-    <Header></Header>
+  <div class="container mx-auto h-full flex justify-center items-center">
+    <div class="w-1/3">
+      <h1 class="font-semibold mb-6 text-center">
+        bitcoin / bitcoin
+      </h1>
 
-    <div v-if='loading' class='py-4'> Loading contributors... </div>
+      <div class="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg">
 
-    <div v-else class='contributors py-4'>
-      <div v-for='c in contributors' :key='c.author.id'>
-        <span> 
-          {{ c.author.login }} with 
-          <span class='font-bold'>{{ c.total }}</span> 
-          commits.
-        </span>
+        <div v-if='loading' class='text-center'>
+          <span class='font-normal'> Fetching top committers... </span>
+        </div>
+
+        <ul class='list-reset' v-else>
+          <li v-for='c in contributors' :key='c.author.id' class='mb-1'>
+            <div class='flex justify-between items-center'>
+              <div> 
+                <img :src='c.author.avatar_url' width='28' height='28' />
+                <span class='font-normal text-xl'> {{c.author.login}} </span>
+              </div>
+
+              <div> 
+                <span class='font-normal text-xl'> {{c.total}} </span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
 import axios from 'axios'
 
 const BASE_URI = 'https://api.github.com'
+const MAX_RECORDS = 10
 
 export default {
   data () {
@@ -37,7 +52,7 @@ export default {
     axios.get(bitcoinUrl)
       .then(response => {
         let contributorsByTopCommits = response.data.reverse()
-        this.contributors = contributorsByTopCommits.slice(0, 5)
+        this.contributors = contributorsByTopCommits.slice(0, MAX_RECORDS)
       })
       .catch(error => {
         console.log(error)
@@ -45,7 +60,6 @@ export default {
       .finally(() => {
         this.loading = false
       })
-  },
-  components: { Header }
+  }
 }
 </script>
