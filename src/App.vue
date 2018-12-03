46f7837dda35d58e584dee86a31ec7b335db6c83
Mark Chavez <markchav3z@gmail.com>
2018-12-03 11:54:48 +0800

    Extract Header and Contributors component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,9 @@
 <template>
   <div class='container mx-auto xs:h-auto h-full flex justify-center items-center'>
-
     <div class='whitebox xs:w-full sm:w-1/2 w-1/3'>
-      <h1 class="font-hairline mb-6 text-center">
-        <span class='font-normal'> bitcoin </span> / 
-        <a :href='bitcoinUrl' class='text-black underline'> bitcoin </a>
-      </h1>
 
-      <div class='border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg'>
-        <div v-if='loading' class='text-center'>
-          <img :src='spinner' width='40' />
-        </div>
-
-        <div v-else>
-          <p class='mb-4 text-center font-hairline text-md tracking-wide uppercase'> All-time contributors </p>
-
-          <ul class='list-reset'>
-            <li v-for='c in contributors' :key='c.author.id' class='mb-2'>
-              <div class='flex justify-between items-center'>
-                <div> 
-                  <a :href='c.author.html_url' class='font-normal text-xl no-underline text-black'>
-                    <img :alt='c.author.id' :src='c.author.avatar_url' width='28' height='28' />
-                    <span class='ml-2 hover:underline'>{{c.author.login}}</span>
-                  </a> 
-                </div>
-
-                <div> 
-                  <span class='font-normal text-xl'> {{format(c.total)}} </span>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Header></Header>
+      <Contributors></Contributors>
 
       <div class="text-center">
         <span class='font-hairline'> 
@@ -54,44 +25,13 @@
 </template>
 
 <script>
-import axios from 'axios'
-import numeral from 'numeral'
-import spinner from '@/assets/images/audio.svg'
-
-const baseUri = 'https://api.github.com'
-const bitcoinUrl = 'https://github.com/bitcoin/bitcoin'
-const maxRecords = 10
+import Header from '@/components/Header'
+import Contributors from '@/components/Contributors'
 
 export default {
-  data () {
-    return {
-      loading: false,
-      spinner: spinner,
-      bitcoinUrl: bitcoinUrl,
-      contributors: []
-    }
-  },
-  created () {
-    let bitcoinUrl = `${baseUri}/repos/bitcoin/bitcoin/stats/contributors`
-
-    this.loading = true
-
-    axios.get(bitcoinUrl)
-      .then(response => {
-        let contributorsByTopCommits = response.data.reverse()
-        this.contributors = contributorsByTopCommits.slice(0, maxRecords)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-      .finally(() => {
-        this.loading = false
-      })
-  },
-  methods: {
-    format (number) {
-      return numeral(number).format('0,0')
-    }
+  components: {
+    Header,
+    Contributors,
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="font-hairline mb-6 text-center">
+    <span class='font-normal'> bitcoin </span> / 
+    <a 
+      href='https://github.com/bitcoin/bitcoin' 
+      class='text-black underline'> 
+      bitcoin 
+    </a>
+  </h1>
+</template>
