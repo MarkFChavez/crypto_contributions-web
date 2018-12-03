2ec24a81a29e9a706d5120c7ab3adfed78b823aa
Mark Chavez <markchav3z@gmail.com>
2018-12-03 11:56:22 +0800

    Extract Footer component

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,21 @@
       <Header></Header>
       <Contributors></Contributors>
 
-      <div class="text-center">
-        <span class='font-hairline'> 
-          Made by 
-          <a href='https://twitter.com/MarkFChavez' class='text-black underline'>Mark</a>.
-        </span>
+      <Footer></Footer>
 
-        <div class='mt-2'>
-          <a 
-            class='github-button' 
-            href='https://github.com/mrkjlchvz/crypto_contributions-web' 
-            data-size='large' 
-            data-show-count='true' 
-            aria-label='Star mrkjlchvz/crypto_contributions-web on GitHub'>Star</a>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import Contributors from '@/components/Contributors'
 
 export default {
   components: {
     Header,
+    Footer,
     Contributors,
   }
 }
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+    <span class='font-hairline'> 
+      Made by 
+      <a href='https://twitter.com/MarkFChavez' class='text-black underline'>Mark</a>.
+    </span>
+
+    <div class='mt-2'>
+      <a 
+        class='github-button' 
+        href='https://github.com/mrkjlchvz/crypto_contributions-web' 
+        data-size='large' 
+        data-show-count='true' 
+        aria-label='Star mrkjlchvz/crypto_contributions-web on GitHub'>Star</a>
+    </div>
+  </div>
+</template>
